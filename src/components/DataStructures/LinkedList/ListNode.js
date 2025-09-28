af880da2c67a020f9f0118d4ea1ddbc5f3db3ed6
Fan <johndqcie2021@163.com>
2025-09-28 18:10:50 +0800

    WIP: local changes before pull

--- src/components/DataStructures/LinkedList/ListNode.js
+++ src/components/DataStructures/LinkedList/ListNode.js
@@ -9,6 +9,7 @@ export default class ListNode{
         this.patched = 0;
         this.selected = 0;
 
+
         // add new values: Visiable Controll
         // Node can be hidden now
         this.hidden = false;    // Hidden node
